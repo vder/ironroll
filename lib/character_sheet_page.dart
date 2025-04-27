84760853d885f3b09979954f43b9476260775520
Piotr Fałdrowicz <pfl@touk.pl>
2025-04-27 15:40:50 +0200

    new icon

--- lib/character_sheet_page.dart
+++ lib/character_sheet_page.dart
@@ -244,47 +244,35 @@ class _CharacterSheetPageState extends State<CharacterSheetPage> {
                         ),
                       ),
                     ),
+                    ConditionMeters(),
                     ProgressTrackList(),
-
                     const SizedBox(height: 16),
                     // Footer Controls
-                    Card(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          VerticalTrack(
-                            label: 'Momentum',
-                            min: -6,
-                            current: 2,
-                            max: 10,
-                          ),
-                          VerticalTrack(
-                            label: 'Health',
-                            min: 0,
-                            current: 4,
-                            max: 5,
-                          ),
-                          VerticalTrack(
-                            label: 'Spirit',
-                            min: 0,
-                            current: 3,
-                            max: 5,
-                          ),
-                          VerticalTrack(
-                            label: 'Supply',
-                            min: 0,
-                            current: 3,
-                            max: 5,
-                          ),
-                        ],
-                      ),
-                    ),
                   ],
                 ),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ConditionMeters extends StatelessWidget {
+  const ConditionMeters({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          VerticalTrack(label: 'Momentum', min: -6, current: 2, max: 10),
+          VerticalTrack(label: 'Health', min: 0, current: 4, max: 5),
+          VerticalTrack(label: 'Spirit', min: 0, current: 3, max: 5),
+          VerticalTrack(label: 'Supply', min: 0, current: 3, max: 5),
+        ],
       ),
     );
   }
